b4443459b10004a43e81605052a960108430165f
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2023-01-18 17:15:48 +0700

    a

--- del-ssh.sh
+++ del-ssh.sh
@@ -9,20 +9,20 @@ echo -e "\E[0;41;36m               DELETE USER                \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
 
-read -p "Username SSH to Delete : " Login
+read -p "User: " -e user
 
 if [ "$os_name" == "Ubuntu" ]
 then
-     killall -9 -u $Login > /dev/null 2>&1
-         userdel -f -r $Login > /dev/null 2>&1
+         killall -9 -u $user > /dev/null 2>&1
+         userdel -f -r $user > /dev/null 2>&1
 
 elif [ "$os_name" == "Debian" ]
 then
-    pkill -u $Login > /dev/null 2>&1
-        userdel -f -r $Login > /dev/null 2>&1
+        pkill -u $user > /dev/null 2>&1
+        userdel -f -r $user > /dev/null 2>&1
 
 elif [ "$os_name" == "CentOS" ]
 then
-        pkill -u $Login > /dev/null 2>&1
-        userdel $Login > /dev/null 2>&1
+        pkill -u $user > /dev/null 2>&1
+        userdel $user > /dev/null 2>&1
 fi
