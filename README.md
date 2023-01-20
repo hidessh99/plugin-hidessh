61e852b6495f9126dd07717cfb8ac9f2c2429c60
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2023-01-20 17:42:02 +0700

    a

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
  
 #plugin auto installer SSH by hide Konek di Panel HideSSH
 wget https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/plugin-panel-ssh.sh;chmod +x plugin-panel-ssh.sh;./plugin-panel-ssh.sh
+
+
+
+
+#plugin fightertunnel
+wget https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/fightertunnel/pckg-fight.sh;chmod +x pckg-fight.sh;./pckg-fight.sh
--- fightertunnel/pckg-fight.sh
+++ fightertunnel/pckg-fight.sh
@@ -9,11 +9,11 @@ wget -O /usr/local/bin/del-ssh-user "https://raw.githubusercontent.com/hidessh99
 
 
 #add vmess
-wget -O /usr/local/bin/add-vmess-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/add-vmess.sh" && chmod +x /usr/local/bin/add-vmess-user
+wget -O /usr/local/bin/add-vmess-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/fightertunnel/add-vmees.sh" && chmod +x /usr/local/bin/add-vmess-user
 
 #delete vmess
-wget -O /usr/local/bin/del-vmess-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/del-vmess.sh" && chmod +x /usr/local/bin/del-vmess-user
+wget -O /usr/local/bin/del-vmess-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/hide/del-v2ray-user.sh" && chmod +x /usr/local/bin/del-vmess-user
 #add trojan
-wget -O /usr/local/bin/add-trojan-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/add-trojan.sh" && chmod +x /usr/local/bin/add-trojan-user
+wget -O /usr/local/bin/add-trojan-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/fightertunnel/add-trojan.sh" && chmod +x /usr/local/bin/add-trojan-user
 #delete trojan
-wget -O /usr/local/bin/del-trojan-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/del-trojan.sh" && chmod +x /usr/local/bin/del-trojan-user
+wget -O /usr/local/bin/del-trojan-user "https://raw.githubusercontent.com/hidessh99/Package-tambahan-Seller/main/hide/del-trojango-user.sh" && chmod +x /usr/local/bin/del-trojan-user
