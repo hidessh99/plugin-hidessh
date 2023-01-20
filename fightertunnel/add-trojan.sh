e27a928ffe2ae2ad288fd6df3dc18324452e542b
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2023-01-20 17:37:20 +0700

    a

---
+++ fightertunnel/add-trojan.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+
+
+clear
+source /var/lib/scrz-prem/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/xray/domain)
+else
+domain=$IP
+fi
+tr="$(cat ~/log-install.txt | grep -w "Trojan WS " | cut -d: -f2|sed 's/ //g')"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
+		read -rp "User: " -e user
+		user_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
+		if [ ${user_EXISTS} -gt '1' ]; then
+		clear
+			echo "A client with the specified name was already created, please choose another name."
+			exit 1
+		fi
+	done
+
+read -p "Password (UUID): " uuid
+
+if [[ "$uuid" == "" ]]; then
+ echo "Password is required!"
+ exit 1
+fi
+
+masaaktif=365
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#trojanws$/a\#! '"$user $exp"'\
+},{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
+sed -i '/#trojangrpc$/a\#! '"$user $exp"'\
+},{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
+
+systemctl restart xray
+trojanlink1="trojan://${uuid}@${domain}:${tr}?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=${domain}#${user}"
+trojanlink="trojan://${uuid}@${domain}:${tr}?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
+clear
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+echo -e "       UUID TROJAN ACCOUNT         " | tee -a /etc/log-create-user.log
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+echo -e "Remarks : ${user}" | tee -a /etc/log-create-user.log
+echo -e "Host/IP : ${domain}" | tee -a /etc/log-create-user.log
+echo -e "port : ${tr}" | tee -a /etc/log-create-user.log
+echo -e "Key : ${uuid}" | tee -a /etc/log-create-user.log
+echo -e "Path : /trojan-ws" | tee -a /etc/log-create-user.log
+echo -e "ServiceName : trojan-grpc" | tee -a /etc/log-create-user.log
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+echo -e "Link WS : ${trojanlink}" | tee -a /etc/log-create-user.log
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+echo -e "Link GRPC : ${trojanlink1}" | tee -a /etc/log-create-user.log
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
+echo "" | tee -a /etc/log-create-user.log
---
+++ fightertunnel/pckg-fight.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#!/bin/bash
+
+#add akun SSH
+wget -O /usr/local/bin/add-ssh-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/add-ssh.sh" && chmod +x /usr/local/bin/add-ssh-user
+#delete akun SSH
+wget -O /usr/local/bin/del-ssh-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/del-ssh.sh" && chmod +x /usr/local/bin/del-ssh-user
+
+
+#add vmess
+wget -O /usr/local/bin/add-vmess-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/add-vmess.sh" && chmod +x /usr/local/bin/add-vmess-user
+
+#delete vmess
+wget -O /usr/local/bin/del-vmess-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/del-vmess.sh" && chmod +x /usr/local/bin/del-vmess-user
+#add trojan
+wget -O /usr/local/bin/add-trojan-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/add-trojan.sh" && chmod +x /usr/local/bin/add-trojan-user
+#delete trojan
+wget -O /usr/local/bin/del-trojan-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/del-trojan.sh" && chmod +x /usr/local/bin/del-trojan-user
