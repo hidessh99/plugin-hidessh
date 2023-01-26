02ec9641e53dd5deeaae147546e0964525751e6a
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2023-01-26 22:50:08 +0700

    a

---
+++ potato-test.sh
@@ -0,0 +1,7 @@
+# SSH
+wget -O /usr/local/bin/add-ssh-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/add-ssh.sh" && chmod +x /usr/local/bin/add-ssh-user
+wget -O /usr/local/bin/del-ssh-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/del-ssh.sh" && chmod +x /usr/local/bin/del-ssh-user
+
+# vmess
+wget -O /usr/local/bin/add-vmess-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/reseller/add-vmess-rel.sh" && chmod +x /usr/local/bin/add-vmess-user
+wget -O /usr/local/bin/del-vmess-user "https://raw.githubusercontent.com/hidessh99/plugin-hidessh/main/reseller/del-vmess-rel.sh" && chmod +x /usr/local/bin/del-vmess-user
