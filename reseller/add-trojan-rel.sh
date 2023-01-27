753b087bfdb0f5ed8b8d4a1a8ebb2a0a50a13c3e
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2023-01-27 17:25:32 +0700

    a

--- reseller/add-trojan-rel.sh
+++ reseller/add-trojan-rel.sh
@@ -47,8 +47,8 @@ echo -e "Remarks : ${user}" | tee -a /etc/log-create-user.log
 echo -e "Host/IP : ${domain}" | tee -a /etc/log-create-user.log
 echo -e "port : ${tr}" | tee -a /etc/log-create-user.log
 echo -e "Key : ${uuid}" | tee -a /etc/log-create-user.log
-echo -e "Path : /trojan" | tee -a /etc/log-create-user.log
-echo -e "ServiceName : trojan-grpc" | tee -a /etc/log-create-user.log
+echo -e "Path WS : /trojan" | tee -a /etc/log-create-user.log
+echo -e "Path GPRC : trojan-grpc" | tee -a /etc/log-create-user.log
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
 echo -e "Link WS : ${trojanlink}" | tee -a /etc/log-create-user.log
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
