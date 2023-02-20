fc690ae060b12ab5efa77bfe3d492e8d971de76c
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2023-02-20 22:52:59 +0700

    a

--- del-ssh.sh
+++ del-ssh.sh
@@ -7,16 +7,16 @@ then
         deluser=$(ps aux | grep "${user} \[priv\]" | sort -k 72 | awk '{print $2}')
         kill "${deluser}"
         userdel -f -r $user > /dev/null 2>&1
-        systemctl restart ssh; systemctl restart sshd
+        systemctl restart ssh; systemctl restart sshd > /dev/null 2>&1
 elif [ "$os_name" == "Debian" ]
 then
         deluser=$(ps aux | grep "${user} \[priv\]" | sort -k 72 | awk '{print $2}')
         kill "${deluser}"
         userdel -f -r $user > /dev/null 2>&1
-        systemctl restart ssh; systemctl restart sshd
+        systemctl restart ssh; systemctl restart sshd > /dev/null 2>&1
 elif [ "$os_name" == "CentOS" ]
 then
         pkill -u $user > /dev/null 2>&1
         userdel $user > /dev/null 2>&1
-        systemctl restart ssh; systemctl restart sshd
+        systemctl restart ssh; systemctl restart sshd > /dev/null 2>&1
 fi
