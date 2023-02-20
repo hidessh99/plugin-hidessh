b99528959e476bc426c50a07ea74547d6048e348
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2023-02-20 21:58:42 +0700

    a

--- del-ssh.sh
+++ del-ssh.sh
@@ -18,7 +18,8 @@ then
 
 elif [ "$os_name" == "Debian" ]
 then
-        pkill -u $user > /dev/null 2>&1
+        deluser=$(ps aux | grep "${user} \[priv\]" | sort -k 72 | awk '{print $2}')
+        kill "${deluser}"
         userdel -f -r $user > /dev/null 2>&1
 
 elif [ "$os_name" == "CentOS" ]
--- pckg3-hide/add-trojan.sh
+++ pckg3-hide/add-trojan.sh
@@ -46,10 +46,10 @@ echo -e "━━━━━━━━━━━━━━━━━━━━━━━�
 echo -e "        TROJAN ACCOUNT         " | tee -a /etc/log-create-user.log
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
 echo -e "Remarks : ${user}" | tee -a /etc/log-create-user.log
-echo -e "Host/IP : ${domain}" | tee -a /etc/log-create-user.log
+4echo -e "Host/IP : ${domain}" | tee -a /etc/log-create-user.log
 echo -e "port : ${tls}" | tee -a /etc/log-create-user.log
 echo -e "Key : ${uuid}" | tee -a /etc/log-create-user.log
-echo -e "Path : ${pathtrojan}" | tee -a /etc/log-create-user.log
+7 echo -e "Path : ${pathtrojan}" | tee -a /etc/log-create-user.log
 echo -e "ServiceName : ${pathtrojangprc}" | tee -a /etc/log-create-user.log
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a /etc/log-create-user.log
 echo -e "Link WS : ${trojanlink}" | tee -a /etc/log-create-user.log
