7c7992d3ed0bea4aa40679859573df061f216bc0
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2023-02-20 22:04:05 +0700

    a

--- del-ssh.sh
+++ del-ssh.sh
@@ -13,8 +13,9 @@ read -p "User: " -e user
 
 if [ "$os_name" == "Ubuntu" ]
 then
-         killall -9 -u $user > /dev/null 2>&1
-         userdel -f -r $user > /dev/null 2>&1
+        deluser=$(ps aux | grep "${user} \[priv\]" | sort -k 72 | awk '{print $2}')
+        kill "${deluser}"
+        userdel -f -r $user > /dev/null 2>&1
 
 elif [ "$os_name" == "Debian" ]
 then
